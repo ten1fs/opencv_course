08c31e4a7c7d22995717c77c5e37d01912345844
tenifs <ten1fs@outlook.com>
2023-01-07 14:08:26 +0800

    project1-3

---
+++ Project1/main.cpp
@@ -0,0 +1,92 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+
+using namespace std;
+using namespace cv;
+
+
+vector<vector<int>> myColors = { 
+	{124, 48, 117, 143, 178, 255}, // purple
+	{68, 72, 156, 107, 126, 255}  // green
+};
+vector<Scalar> myColorValues = { 
+	{255, 0, 255}, // purple
+	{0, 255, 0}    // green
+};
+Mat img;
+vector<vector<int>> newPoints;
+
+
+Point getContours(Mat imgDil)
+{
+	Point myPoint(0, 0);
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	//drawContours(img, contours, -1, Scalar(255, 0, 255), 10);
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+	string objectType;
+	for (int i = 0; i < contours.size(); i++)
+	{
+		int area = contourArea(contours[i]);
+		//cout << area << endl;
+		if (area > 1000)
+		{
+			float peri = arcLength(contours[i], true);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+			//cout << conPoly[i].size() << endl;
+			boundRect[i] = boundingRect(conPoly[i]);
+			myPoint.x = boundRect[i].x + boundRect[i].width / 2;
+			myPoint.y = boundRect[i].y;
+
+			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+		}
+	}
+	return myPoint;
+}
+
+
+void findColor()
+{
+	Mat imgHSV;
+	cvtColor(img, imgHSV, COLOR_BGR2HSV);
+	for (int i = 0; i < myColors.size(); i++)
+	{
+		Scalar lower(myColors[i][0], myColors[i][1], myColors[i][2]);
+		Scalar upper(myColors[i][3], myColors[i][4], myColors[i][5]);
+		Mat mask;
+		inRange(imgHSV, lower, upper, mask);
+		//imshow(to_string(i), mask);
+		Point myPoint = getContours(mask);
+		if (myPoint.x != 0 && myPoint.y != 0)
+			newPoints.push_back({ myPoint.x, myPoint.y, i});
+	}
+}
+
+void drawOnCanvas()
+{
+	for (int i = 0; i < newPoints.size(); i++)
+	{
+		circle(img, Point(newPoints[i][0], newPoints[i][1]), 10, myColorValues[newPoints[i][2]], FILLED);
+	}
+}
+
+
+int main()
+{
+	VideoCapture cap(0);
+	while (true)
+	{
+		cap.read(img);
+		findColor();
+		drawOnCanvas();
+		imshow("Image", img);
+		waitKey(1);
+	}
+	return 0;
+}
